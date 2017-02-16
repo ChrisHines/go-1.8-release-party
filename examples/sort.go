139275567deb4452ad00d02c073d7ab42ab2e168
Chris Hines <christopher_hines@comcast.com>
2017-02-16 15:41:06 -0500

    Customizations for Golang Reston Meetup.

--- examples/sort.go
+++ examples/sort.go
@@ -1,31 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"sort"
-	"strings"
 )
 
 func main() {
-	switch strings.ToLower(field) {
-	case "updated":
-		sort.Slice(subs, func(i, j int) bool { return subs[i].Updated.After(subs[j].Updated) })
-	case "trust":
-		sort.Slice(subs, func(i, j int) bool { return subs[i].Trust > subs[j].Trust })
-	case "stddev":
-		sort.Slice(subs, func(i, j int) bool {
-			var s1, s2 stats.Sample
-			for _, r := range subs[i].Ratings {
-				s1.Xs = append(s1.Xs, float64(r.Value))
-			}
-			for _, r := range subs[j].Ratings {
-				s2.Xs = append(s2.Xs, float64(r.Value))
-			}
-			return s1.StdDev() < s2.StdDev()
-		})
-	case "rating":
-		fallthrough
-	default:
-		sort.Slice(subs, func(i, j int) bool { return subs[i].Rating > subs[j].Rating })
-
+	people := []struct {
+		Name string
+		Age  int
+	}{
+		{"Gopher", 7},
+		{"Alice", 55},
+		{"Vera", 24},
+		{"Bob", 75},
 	}
+
+	sort.Slice(people, func(i, j int) bool { return people[i].Name < people[j].Name })
+	fmt.Println("By name:", people)
+
+	sort.Slice(people, func(i, j int) bool { return people[i].Age < people[j].Age })
+	fmt.Println("By age:", people)
 }
